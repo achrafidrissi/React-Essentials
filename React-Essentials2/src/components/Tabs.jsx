1fbbad3e5eaf4d8a3c666cc335f1c6138e9e1869
achrafidrissi <achrafidrissi05@gmail.com>
2024-07-29 12:54:19 +0100

    New commit

---
+++ React-Essentials2/src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Tabs({children, buttons, buttonsContainer = "menu"}){
+    const ButtonContainer = buttonsContainer
+    return(<>
+
+        <ButtonContainer>
+            {buttons}
+        </ButtonContainer>
+        {children}
+    </>
+    )
+}
